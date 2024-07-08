a5ad7f607685b1d608603431ae267eb61f02ab15
“Trainer0064” <trainer0064@gmail.com>
2024-07-08 12:56:13 +0530

    Templates

--- Django/sit/project/playground/views.py
+++ Django/sit/project/playground/views.py
@@ -4,12 +4,65 @@ from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
 def first_api(request):
-    return HttpResponse("Hello, This is our first api using Django Framework")
+    return HttpResponse("<h1>Hello, This is our first api using Django Framework</h1>")
 
 def json_api(request):
     data = {
         'name': 'John Doe',
         'age': 30,
-        'city': 'New York'
+        'city': 'New York',
+        36:"strength"
     }
     return JsonResponse(data)
+
+def dynamic_api(request, name):
+    if name == "SIT":
+        name = "Silicon University"
+    print(type(name))
+    return HttpResponse(f"<h1>Hello, {name}!</h1>")
+
+std_data = {
+    1	:	"ADITYESWAR  SAHU",
+2	:	"ARPIT  SARANGI",
+3	:	"DOLAMANI  MEHER",
+4	:	"SANJANA  MOHANTA",
+5	:	"AKANKSHA  BARIK",
+}
+
+def student_api(request, roll_no):
+    if roll_no in std_data:
+        return HttpResponse(f"<h1>Hello, {std_data[roll_no]}!</h1>")
+    else:
+        return HttpResponse("<h1>Student not found.</h1>")
+
+# def student_api2(request, str_data):
+#     return HttpResponse(str_data)
+
+def test_html(request,attendance):
+    data = {
+        'name': 'Silicon',
+        'age': 12,
+        'city': 'BBSR',
+        "strength":36,
+        "lst":[
+            "ADITYESWAR  SAHU","ARPIT  SARANGI","DOLAMANI  MEHER","SANJANA  MOHANTA","AKANKSHA  BARIK",
+        ]
+    }
+    return render(request, 'index.html',context={"class_data":data,"attendance":attendance})
+
+
+"""
+Portfolio Views
+"""
+
+def portfolio_home(request):
+    return render(request, 'home.html')
+
+def portfolio_about(request):
+    return render(request, 'about.html')
+
+def portfolio_projects(request):
+    return render(request, 'projects.html')
+
+def portfolio_skills(request):
+    return render(request, 'skills.html')
